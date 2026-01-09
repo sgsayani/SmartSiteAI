2f285ed43bf35a0c2881e13b4fccb8d331bcc77d
sgsayani <sgsayanighatak@gmail.com>
2026-01-10 00:12:31 +0530

    api integration for preview and projects

--- src/pages/MyProjects.tsx
+++ src/pages/MyProjects.tsx
@@ -2,38 +2,59 @@ import { useEffect, useState } from "react";
 import type { Project } from "../types";
 import { Loader2Icon, PlusIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import { dummyProjects } from "../assets/assets";
+// import { dummyProjects } from "../assets/assets";
 import Footer from "../components/Footer";
 import api from "@/configs/axios";
 import { toast } from "sonner";
+import { authClient } from "@/lib/auth-client";
 
 const MyProjects = () => {
+  const {data: session, isPending} = authClient.useSession();
   const [loading, setLoading] = useState(true);
   const [projects, setProjects] = useState<Project[]>([]);
   const navigate = useNavigate();
 
-  const fetchProjects = async() =>{
+  const fetchProjects = async () => {
     try {
-      const{data} = await api.get('/api/user/projects')
-      setProjects(data.projects)
-      setLoading(false)
-    } catch (error:any) {
+      const { data } = await api.get("/api/user/projects");
+      setProjects(data.projects);
+      setLoading(false);
+    } catch (error: any) {
       console.log(error);
       toast.error(error?.response?.data?.message || error.message);
-      
     }
-  }
-
-  
+  };
 
   const deleteProject = async (projectId: string) => {
     console.log("Delete project:", projectId);
+    try {
+      const confirm = window.confirm('Are you sure you want to delete this project?');
+      if(!confirm) return;
+      const { data } = await api.delete(`/api/user/projects/${projectId}`);
+      toast.success(data.message);
+      fetchProjects();
+      
+    } catch (error:any) {
+      console.log(error);
+      toast.error(error?.response?.data?.message || error.message);
+
+      
+    }
   };
 
   useEffect(() => {
-    setProjects(dummyProjects);
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
+    if(session?.user && !isPending){
+    fetchProjects();
+    }else if(!isPending && !session?.user){
+      navigate('/');
+      toast('Please login to view your projects');
+    }
+  }, [session?.user]);
+
+  // useEffect(() => {
+  //   setProjects(dummyProjects);
+  //   setTimeout(() => setLoading(false), 1000);
+  // }, []);
 
   return (
     <>
